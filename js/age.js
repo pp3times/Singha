6326706cdb19789e01987f505c2dad551a753279
Thanawat <thanawat.j2314@hotmail.com>
2021-11-13 19:34:27 +0700

    :wrench: refactor

--- js/age.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('form select').on('change', function() {
-        if ($(this).find('option:selected').val() != '-1')
-            $(this).addClass('active');
-        else
-            $(this).removeClass('active');
-    });
-});
